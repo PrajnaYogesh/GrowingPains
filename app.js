eb9f0b722fc8fd1b332849e1e793b6ffc0037d93
Prajna <prajna.yogish4@gmail.com>
2024-07-26 09:12:43 -0400

    try catch added

--- app.js
+++ app.js
@@ -15,29 +15,36 @@ console.log(`Area occupied by 20 plants on day 1 is ${spaceOccupied}`)
 
 let week = prompt("Enter the week number ");
 
-for(let i=0;i<week;i++){
-noOfPlants= noOfPlants*2;
-}
-console.log(`Totatl no of plants after ${week} is ${noOfPlants}`);
-// latest space occupied 
-spaceOccupied=noOfPlants*spaceForOnePlant;
+try{
+    if(week == 100) {
+         throw new Error('Incorrect number of week'); 
+    }
+    for(let i=0;i<week;i++){
+        noOfPlants= noOfPlants*2;
+        }
+        console.log(`Totatl no of plants after ${week} is ${noOfPlants}`);
+        // latest space occupied 
+        spaceOccupied=noOfPlants*spaceForOnePlant;
+        
+        console.log(`Latest space occupied after ${week} week is ${spaceOccupied}`);
+        
+        let pruneCutOff = (areaOfCircle * 80) / 100;
+        console.log(pruneCutOff);
+        
+        let monitorCutOff = (areaOfCircle*50)/100;
+        console.log(monitorCutOff);
+        
+        if(spaceOccupied < monitorCutOff){
+            console.log("Plant more !!");
+        } else if(spaceOccupied > monitorCutOff && spaceOccupied < pruneCutOff){
+            console.log("It's time to monitor!!");
+        
+        }else{
+        console.log("Time to Prune!!")
+        }
 
-console.log(`Latest space occupied after ${week} week is ${spaceOccupied}`);
 
-let pruneCutOff = (areaOfCircle * 80) / 100;
-console.log(pruneCutOff);
 
-let monitorCutOff = (areaOfCircle*50)/100;
-console.log(monitorCutOff);
-
-if(spaceOccupied < monitorCutOff){
-    console.log("Plant more !!");
-} else if(spaceOccupied > monitorCutOff && spaceOccupied < pruneCutOff){
-    console.log("It's time to monitor!!");
-
-}else{
-console.log("Time to Prune!!")
-}
 
 // Part 2 -  for 100 plants space required
 
@@ -54,7 +61,10 @@ for(let i=0;i<10;i++){
 let rad = Math.sqrt((plants100 * 0.8)/3.14);
 console.log(rad);
 
-
+        
+}catch(e){
+    console.log(e);
+    }
 
 
 
